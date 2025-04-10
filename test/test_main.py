4f0d92da1421ac5232eb81a7f388204200d3027c
Ian <yu66631@e-united.com.tw>
2025-04-10 15:15:50 +0800

    test for flask fail

--- test/test_main.py
+++ test/test_main.py
@@ -1,10 +1,12 @@
 from app.main import app
 
+
 def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
     assert b"Hello, Ian!" in response.data
+
 
 def test_echo():
     client = app.test_client()
@@ -12,8 +14,9 @@ def test_echo():
     assert response.status_code == 200
     assert response.get_json() == {"message": "Hi"}
 
+
 def test_add():
     client = app.test_client()
     response = client.get("/api/add/5/7")
     assert response.status_code == 200
-    assert response.get_json() == {"result": 12}
+    assert response.get_json() == {"result": 1772}
